b1cb88a9b60bde283b7caf4d9eaa552b82b3026a
EgorRU <sergeyrootuser@gmail.com>
2023-11-16 11:18:05 +0300

    edit tutor.py

--- app/tutor.py
+++ app/tutor.py
@@ -9,6 +9,9 @@ router_tutor = Router()
 
 keyboard = InlineKeyboardMarkup(inline_keyboard=[
         [InlineKeyboardButton(text='Сводная таблица', url='https://docs.google.com/spreadsheets/d/1QAD5LnGSAaSBU8Q4EO8dRPqeoeQkKD2VmoaWrjJDiUk/edit')],
+        [InlineKeyboardButton(text='Таблица с корпоративными почтами', url='https://docs.google.com/spreadsheets/d/15ywquVtsVj19oq8UoUuaHhPAY8um0XHg/edit')],
+        [InlineKeyboardButton(text='Ссылка на ассесмент(НЕ IT)', url='https://testing.assessment.unionepro.ru/courses/6537ebcee4d75719e8c430d4')],
+        [InlineKeyboardButton(text='Ссылка на ассесмент(IT)', url='https://testing.assessment.unionepro.ru/courses/6537ed18e4d75719e8c430da')],
         [InlineKeyboardButton(text='Меню', callback_data='Меню')]
         ])
 
